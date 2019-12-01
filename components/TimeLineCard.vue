72ffae88d918bb06f341513316218863452b850c
Lima <lmimsra07@gmail.com>
2019-12-01 23:58:04 +0900

    ダミーのファイル追加、タイムライン作成

---
+++ components/TimeLineCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="card-spacer">
+    <div class="card">
+      <div class="card-content">
+        <div class="media media-header">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <!--src は画像固定URL/Userに紐づく何かで取れるようにしておく。今はサンプル-->
+              <img
+                src="https://bulma.io/images/placeholders/96x96.png"
+                alt="Placeholder image"
+                class="is-rounded"
+              />
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="item-title">
+              {{ cardContent.title }}
+            </p>
+            <p class="subtitle is-6">
+              <span>
+                {{ cardContent.owner }}
+              </span>
+              <span>(@{{ cardContent.ownerId }})</span>
+            </p>
+            <div class="content">
+              <!-- メインコンテンツ -->
+              {{ cardContent.comment }}
+            </div>
+            <p>
+              <img
+                src="https://bulma.io/images/placeholders/96x96.png"
+                alt="Placeholder image"
+              />
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimeLineCard',
+  props: {
+    cardContent: { type: Object, required: true }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.card-spacer {
+  margin: 0.5rem 0;
+}
+.card-content {
+  padding: 0.5rem;
+}
+.media-header {
+  margin-bottom: 0 !important;
+}
+.subtitle {
+  margin-bottom: 0 !important;
+  padding-bottom: 0 !important;
+}
+.media-content .content {
+  line-height: 1.25;
+}
+.item-title {
+  font-weight: bold;
+}
+.owner-text {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
--- pages/Home.vue
+++ pages/Home.vue
@@ -1,13 +1,164 @@
 <template>
   <div>
     <h1>Homeコンポーネント</h1>
+    <TimeLineCard
+      v-for="(item, index) in timeLineItem"
+      :key="index"
+      :card-content="item"
+      style="min-width: 100%"
+    />
   </div>
 </template>
 
 <script>
+import TimeLineCard from '@/components/TimeLineCard'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    TimeLineCard
+  },
+  data: () => {
+    return {
+      timeLineItem: [
+        {
+          id: 111,
+          owner: 'test1',
+          ownerId: '12345667',
+          title: 'テスト投稿',
+          comment: '全ユーザーが見れるコメント',
+          publishTime: '2019-05-09 12:30:00',
+          limitTime: '2019-09-09 12:30:00',
+          participants: [
+            { id: 123, name: 'おらおら', status: 3 },
+            { id: 223, name: 'われわれ', status: 1 }
+          ]
+        },
+        {
+          id: 112,
+          owner: 'test2',
+          ownerId: '2312',
+          title: 'テスト投稿',
+          comment: '全ユーザーが見れるコメント',
+          publishTime: '2019-05-09 12:30:00',
+          limitTime: '2019-10-09 12:30:00',
+          participants: [
+            { id: 123, name: 'おらおら', status: 3 },
+            { id: 223, name: 'われわれ', status: 1 }
+          ]
+        },
+        {
+          id: 112,
+          owner: 'test2',
+          ownerId: '2312',
+          title: 'テスト投稿',
+          comment: '全ユーザーが見れるコメント',
+          publishTime: '2019-05-09 12:30:00',
+          limitTime: '2019-10-09 12:30:00',
+          participants: [
+            { id: 123, name: 'おらおら', status: 3 },
+            { id: 223, name: 'われわれ', status: 1 }
+          ]
+        },
+        {
+          id: 112,
+          owner: 'test2',
+          ownerId: '2312',
+          title: 'テスト投稿',
+          comment: '全ユーザーが見れるコメント',
+          publishTime: '2019-05-09 12:30:00',
+          limitTime: '2019-10-09 12:30:00',
+          participants: [
+            { id: 123, name: 'おらおら', status: 3 },
+            { id: 223, name: 'われわれ', status: 1 }
+          ]
+        },
+        {
+          id: 112,
+          owner: 'test2',
+          ownerId: '2312',
+          title: 'テスト投稿',
+          comment: '全ユーザーが見れるコメント',
+          publishTime: '2019-05-09 12:30:00',
+          limitTime: '2019-10-09 12:30:00',
+          participants: [
+            { id: 123, name: 'おらおら', status: 3 },
+            { id: 223, name: 'われわれ', status: 1 }
+          ]
+        },
+        {
+          id: 112,
+          owner: 'test2',
+          ownerId: '2312',
+          title: 'テスト投稿',
+          comment: '全ユーザーが見れるコメント',
+          publishTime: '2019-05-09 12:30:00',
+          limitTime: '2019-10-09 12:30:00',
+          participants: [
+            { id: 123, name: 'おらおら', status: 3 },
+            { id: 223, name: 'われわれ', status: 1 }
+          ]
+        },
+        {
+          id: 112,
+          owner: 'test2',
+          ownerId: '2312',
+          title: 'テスト投稿',
+          comment: '全ユーザーが見れるコメント',
+          publishTime: '2019-05-09 12:30:00',
+          limitTime: '2019-10-09 12:30:00',
+          participants: [
+            { id: 123, name: 'おらおら', status: 3 },
+            { id: 223, name: 'われわれ', status: 1 }
+          ]
+        },
+        {
+          id: 112,
+          owner: 'test2',
+          ownerId: '2312',
+          title: 'テストな投稿',
+          comment: '全ユーザーが見れるコメント',
+          publishTime: '2019-05-09 12:30:00',
+          limitTime: '2019-10-09 12:30:00',
+          participants: [
+            { id: 123, name: 'おらおら', status: 3 },
+            { id: 223, name: 'われわれ', status: 1 }
+          ]
+        },
+        {
+          id: 112,
+          owner: 'test2',
+          ownerId: '2312',
+          title: 'テスト投稿',
+          comment: '全ユーザーが見れるコメント',
+          publishTime: '2019-05-09 12:30:00',
+          limitTime: '2019-10-09 12:30:00',
+          participants: [
+            { id: 123, name: 'おらおら', status: 3 },
+            { id: 223, name: 'われわれ', status: 1 }
+          ]
+        },
+        {
+          id: 112,
+          owner: 'test2',
+          ownerId: '2312',
+          title: 'テスト投稿',
+          comment: '全ユーザーが見れるコメント',
+          publishTime: '2019-05-09 12:30:00',
+          limitTime: '2019-10-09 12:30:00',
+          participants: [
+            { id: 123, name: 'おらおら', status: 3 },
+            { id: 223, name: 'われわれ', status: 1 }
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped lang="scss" />
+<style scoped lang="scss">
+.time-line-area {
+  /*横幅は常にmax*/
+  width: 100%;
+}
+</style>
